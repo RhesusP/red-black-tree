8f51652385e8567c1d95e6efe65981c648ce6972
K1000 <camille.bernot@etu.univ-lyon1.fr>
2021-11-18 12:54:12 +0100

    calcul du temps moyen d'insertion des ABR et ARN

--- main.cpp
+++ main.cpp
@@ -1,51 +1,52 @@
 #include "ARN.h"
 #include "ABR.h"
 #include <iostream>
+#include <cstdlib>
+#include <chrono>
+#include <cstdio>
 
 using namespace std;
 
 int main(){
 
-    ARN arbre;
+    int nbC = 100;          //nombre de collection
+    int nbE = 20000;         //nombre d'element
 
-    for(int i=0 ; i<20 ; i++){
-        cout << endl << endl << "Insertion de " << i << endl;
-        arbre.insertion(i);
-        arbre.affichage();
+
+    chrono::time_point<chrono::system_clock> startARN, endARN;
+    startARN = chrono::system_clock::now();
+    for(int i=0; i<nbC ; i++){
+        ARN arbreRougeNoir;
+        for(int j=0 ; j<nbE ; j++){
+            int random = rand()%1000;
+            arbreRougeNoir.insertion(random);
+        }
+        //cout << "Arbre rouge noir " << i << " :" << endl;
+        //arbreRougeNoir.affichage();
     }
+    endARN = chrono::system_clock::now();
+    int elapsedTimeARN = chrono::duration_cast<chrono::microseconds>(endARN-startARN).count(); 
+    float moyenneARN = elapsedTimeARN/nbC;
+    cout << "[ARN] Temps moyen pour une insertion de " << nbE << " elements : " << moyenneARN << " microsecondes." << endl;
 
-    cout << "------------------------------------------------------------" << endl;
 
-    ABR arbreBinaire;
-    
-    for(int i=0 ; i<20 ; i++){
-        arbreBinaire.insertion(i);
+
+    chrono::time_point<chrono::system_clock> startABR, endABR;
+    startABR = chrono::system_clock::now();
+    for(int i=0; i<nbC ; i++){
+        ABR arbreRougeNoir;
+        for(int j=0 ; j<nbE ; j++){
+            int random = rand()%1000;
+            arbreRougeNoir.insertion(random);
+        }
+        //cout << "Arbre rouge noir " << i << " :" << endl;
+        //arbreRougeNoir.affichage();
     }
-    arbreBinaire.affichage();
+    endABR = chrono::system_clock::now();
+    int elapsedTimeABR = chrono::duration_cast<chrono::microseconds>(endABR-startABR).count(); 
+    float moyenneABR = elapsedTimeABR/nbC;
+    cout << "[ABR] Temps moyen pour une insertion de " << nbE << " elements : " << moyenneABR << " microsecondes." << endl;
 
 
-/*
-    ARN test;
-    test.insertion(30);
-    test.affichage();
-    test.insertion(10);
-    test.affichage();
-    test.insertion(7);
-    test.affichage();   
-    test.insertion(4);
-    test.affichage();
-    test.insertion(3);
-    test.affichage();
-    test.insertion(1);
-    test.affichage();
-    test.insertion(6);
-    test.affichage();
-    test.insertion(9);
-    test.affichage();
-    test.insertion(40);
-    test.affichage();
-    test.insertion(60);
-    test.affichage();
-*/
     return 0;
 }
