443cae6feeb1ed3f3cefb4187d2410ef0949ba9d
K1000 <camille.bernot@etu.univ-lyon1.fr>
2021-11-18 11:04:21 +0100

    passage fonctions non essentielles en privé

--- ARN.h
+++ ARN.h
@@ -9,27 +9,25 @@ class ARN{
 
         ARN();  
         ~ARN();
-        void vider(Noeud* node);
+        
         void insertion(const Element &e);
         Noeud* rechercherElement(const Element &e);
         void affichage();
+        
+    
+    private:
+        void affichageRec(Noeud* n, int prof);
+        void vider(Noeud* node);
         Noeud* getNoeudParent(const Noeud* node);
         Noeud* getNoeudGrandParent(const Noeud* node);
         Noeud* getNoeudArriereGrandParent(const Noeud* node);
         Noeud* getNoeudOncle(const Noeud* node);
-
         void equilibrage(Noeud* node);
         void cas1(Noeud* node);
         void cas2(Noeud* node);
-
         void rotationGauche(Noeud* node);
         void rotationDroite(Noeud* node);
-
         void insertionRec(Noeud *&node, const Element &e);
-    
-    private:
-        void affichageRec(Noeud* n, int prof);
-        
     };
 
 #endif
