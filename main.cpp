f7f26d5717179f021ee6334ab5888157574ea455
K1000 <camille.bernot@etu.univ-lyon1.fr>
2021-11-22 22:48:55 +0100

    automatisation performances

--- main.cpp
+++ main.cpp
@@ -4,50 +4,68 @@
 #include <cstdlib>
 #include <chrono>
 #include <cstdio>
+#include <fstream>
 
 using namespace std;
 
 int main(){
+    ofstream flux("./perfs_insertion.txt");
+    if(flux){
+        flux << "# \"nb elmt\" \"Temps ABR (microsecondes)\" \"Temps ARN (microsecondes)\"" << endl;
+        int nbC = 100;               //nombre de collection
+        int nbEMin = 0;
+        int nbEMax = 25000;         //nombre d'element
 
-    int nbC = 10;          //nombre de collection
-    int nbE = 100;         //nombre d'element
+        for(int i=nbEMin ; i<nbEMax+100 ; i+=1000){
+            flux << i << " ";
+                chrono::time_point<chrono::system_clock> startARN, endARN;
+                startARN = chrono::system_clock::now();
+                for(int j=0; j<nbC ; j++){
+                    ARN arbreRougeNoir;
+                    for(int k=0 ; k<i ; k++){
+                        int random = rand()%1000;
+                        arbreRougeNoir.insertion(random);
+                    }
+                    //arbreRougeNoir.affichage();
+                }
+                endARN = chrono::system_clock::now();
+                int elapsedTimeARN = chrono::duration_cast<chrono::microseconds>(endARN-startARN).count(); 
+                float moyenneARN = elapsedTimeARN/nbC;
+                flux << moyenneARN << " ";
+                cout << "[ARN] Temps moyen pour une insertion de " << i << " elements : " << moyenneARN << " microsecondes." << endl;
 
-    chrono::time_point<chrono::system_clock> startARN, endARN;
-    startARN = chrono::system_clock::now();
-    for(int i=0; i<nbC ; i++){
-        ARN arbreRougeNoir;
-        for(int j=0 ; j<nbE ; j++){
-            int random = rand()%1000;
-            arbreRougeNoir.insertion(random);
-        }
-        //arbreRougeNoir.affichage();
+
+                chrono::time_point<chrono::system_clock> startABR, endABR;
+                startABR = chrono::system_clock::now();
+                for(int j=0; j<nbC ; j++){
+                    ABR arbreBinaire;
+                    for(int k=0 ; k<i ; k++){
+                        int random = rand()%1000;
+                        arbreBinaire.insertion(random);
+                    }
+                    //arbreBinaire.affichage();
+                }
+                endABR = chrono::system_clock::now();
+                int elapsedTimeABR = chrono::duration_cast<chrono::microseconds>(endABR-startABR).count(); 
+                float moyenneABR = elapsedTimeABR/nbC;
+                flux << moyenneABR << endl;
+                cout << "[ABR] Temps moyen pour une insertion de " << i << " elements : " << moyenneABR << " microsecondes." << endl;
+                cout << "--------------------------------------------------------------------------------------------------" << endl;
+            }
+
+
+
+
+
+
+
+
+
     }
-    endARN = chrono::system_clock::now();
-    int elapsedTimeARN = chrono::duration_cast<chrono::microseconds>(endARN-startARN).count(); 
-    float moyenneARN = elapsedTimeARN/nbC;
-    cout << "[ARN] Temps moyen pour une insertion de " << nbE << " elements : " << moyenneARN << " microsecondes." << endl;
-
-
-    chrono::time_point<chrono::system_clock> startABR, endABR;
-    startABR = chrono::system_clock::now();
-    for(int i=0; i<nbC ; i++){
-        ABR arbreBinaire;
-        for(int j=0 ; j<nbE ; j++){
-            int random = rand()%1000;
-            arbreBinaire.insertion(random);
-        }
+    else{
+        cout << "ERREUR : impossible d'ouvrir le fichier." << endl;
     }
-    endABR = chrono::system_clock::now();
-    int elapsedTimeABR = chrono::duration_cast<chrono::microseconds>(endABR-startABR).count(); 
-    float moyenneABR = elapsedTimeABR/nbC;
-    cout << "[ABR] Temps moyen pour une insertion de " << nbE << " elements : " << moyenneABR << " microsecondes." << endl;
 
-    ARN arbre;
-    arbre.insertion(10);
-    arbre.insertion(40);
-    arbre.insertion(60);
-    arbre.insertion(20);
-    arbre.affichage();
-
+    
     return 0;
 }
